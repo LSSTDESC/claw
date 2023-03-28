273845729bd46ccd164cfe5a05e6bf665d36d9c2
m-aguena <michel.aguena@gmail.com>
2023-03-28 23:48:30 +0200

    implement properly redshift_proxy/gaussian.py

--- claw/redshift_proxy/gaussian.py
+++ claw/redshift_proxy/gaussian.py
@@ -3,60 +3,31 @@ abstract class to compute cluster redshift functions.
 ========================================
 The implemented functions use PyCCL library as backend.
 """
-from __future__ import annotations
-from typing import List, Optional, final
-from abc import abstractmethod
-
-import pyccl as ccl
 import numpy as np
+from ..utils import gaussian, gaussian_integral
 
 
-class ClusterRedshift:
+class Gaussian:
     """Cluster Redshift module."""
 
     def __init__(
         self,
     ):
-        self.zl = 0.0
-        self.zu = 2.0
-        self.use_proxy = False
+        pass
 
-    def compute_differential_comoving_volume(
-        self, ccl_cosmo: ccl.Cosmology, z
-    ) -> float:
-        """
-        parameters
-        ccl_cosmo : pyccl Cosmology
-        z : float
-            Cluster Redshift.
-        reuturn
-        -------
-        dv : float
-            Differential Comoving Volume at z in units of Mpc^3 (comoving).
-        """
-        a = 1.0 / (1.0 + z)  # pylint: disable=invalid-name
-        # pylint: disable-next=invalid-name
-        da = ccl.background.angular_diameter_distance(ccl_cosmo, a)
-        E = ccl.background.h_over_h0(ccl_cosmo, a)  # pylint: disable=invalid-name
-        dV = (  # pylint: disable=invalid-name
-            ((1.0 + z) ** 2)
-            * (da**2)
-            * ccl.physical_constants.CLIGHT_HMPC
-            / ccl_cosmo["h"]
-            / E
-        )
-        return dV
+    def _get_sigmazobs(self, z, sigma_0):
+        return sigma_0*(z+1.0)
 
-    def set_redshift_limits(self, zl, zu):
-        """Method to set the integration limits of the true redshift."""
-        self.zl = zl
-        self.zu = zu
-        return None
+    def prob_z_zobs(self, z, z_obs, sigma_0):
 
-    @abstractmethod
-    def cluster_z_p(self, ccl_cosmo, logM, z, z_obs, z_obs_params):
-        """Computes the logM proxy"""
+        sigma = self._get_sigmazobs(z, sigma_0)
 
-    @abstractmethod
-    def cluster_z_intp(self, ccl_cosmo, logM, z, z_obs, z_obs_params):
-        """Computes the logM proxy"""
+        return gaussian(z_obs, z, sigma)
+
+    def prob_z_zobs_integ(self, z, z_obs_lim, sigma_0):
+
+        sigma = self._get_sigmazobs(z, sigma_0)
+        x_min = (z_obs_mean - z_obs_lim[0]) / (np.sqrt(2.0) * sigma)
+        x_max = (z_obs_mean - z_obs_lim[1]) / (np.sqrt(2.0) * sigma)
+
+        return gaussian_integral(xmin, xmax)
